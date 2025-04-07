6b606033200849c581fa51920cc085c202eb8a53
Pedro Gutierrez <pedroecgutierrez@gmail.com>
2025-04-07 15:55:08 -0300

    ex002

---
+++ ex002.cpp
@@ -0,0 +1,24 @@
+//Faça um programa que receba três notas, calcule e mostre a média aritmética.
+#include <iostream>
+using namespace std;
+    float n1, n2, n3;
+    
+int main (){
+    cout << "Programa para calculo de medias "<<endl;
+
+    cout << "digite o valor da primeira nota: ";
+    cin >> n1;
+    cout <<  "\ndigite o valor da segunda nota: ";
+    cin >> n2;
+    cout << "\ndigite o valor da terceira nota: ";
+    cin >> n3;
+
+    float media = (n1 + n2 + n3)/3;
+    cout << "\nA media desses tres valores é: " << media;
+
+    
+
+
+
+    return 0;
+}
