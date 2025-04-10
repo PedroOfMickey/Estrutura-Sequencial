641dd95ec169950846e99368b1446e25dd8e44c8
Pedro Gutierrez <pedroecgutierrez@gmail.com>
2025-04-10 12:44:34 -0300

    ex010

---
+++ ex010.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    float Area, p, r;
+
+
+    cout << " Escreva o valor de P: ";
+    cin >> p;
+    cout << "\n Escreva a o valor de R: ";
+    cin >> r;
+
+    Area = p * (r*r);
+
+    cout << "A area do circulo é igual a: " << Area << endl; 
+
+
+
+
+
+
+return 0;
+}
